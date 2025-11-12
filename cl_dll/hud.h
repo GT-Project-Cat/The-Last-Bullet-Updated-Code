cfb9764ee20aeb7374715aa09e83ce82610f94b7
Alivebyte <alivebyte.yt@gmail.com>
2025-11-12 18:13:04 +0300

    Greenish hud color

--- cl_dll/hud.h
+++ cl_dll/hud.h
@@ -22,7 +22,7 @@
 #pragma once
 #if !defined(HUD_H)
 #define HUD_H
-#define RGB_YELLOWISH 0x00FFA000 //255,160,0
+#define RGB_YELLOWISH 0x0000A000 //255, 218, 185 
 #define RGB_REDISH 0x00FF1010 //255,160,0
 #define RGB_GREENISH 0x0000A000 //0,160,0
 
